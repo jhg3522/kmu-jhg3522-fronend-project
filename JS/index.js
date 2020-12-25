718810a7dd193a2a756755ea1b4acb6a97c09385
jhg3522 <jhg3522@kookmin.ac.kr>
2020-12-25 18:35:52 +0900

    slider jquery

--- JS/index.js
+++ JS/index.js
@@ -12,3 +12,24 @@ $(function() {
     })
 
 });
+
+$(function() {
+    var $slider = $("#slider");
+    var $slides = $slider.find('.slides');
+    var $slide = $slides.find('.slide');
+
+    var currentSlide = 1;
+
+    setInterval(function(){
+
+        $slides.animate({'margin-left': "-=" + 1024},1000,function(){
+            currentSlide++;
+            if(currentSlide === $slide.length){
+                currentSlide=1;
+                $slides.css("margin-left",0);
+            }
+        }) //1024 이미지의 크기 의미
+        //animation 움직이는 시간 1초
+    }, 3000) //3초
+
+});
